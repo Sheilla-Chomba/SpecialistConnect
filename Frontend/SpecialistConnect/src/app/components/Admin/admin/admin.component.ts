ffbfcafaa0676ae91ff518b6626f644c41d54125
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-14 16:24:54 +0300

    Admin completed

--- Frontend/SpecialistConnect/src/app/components/Admin/admin/admin.component.ts
+++ Frontend/SpecialistConnect/src/app/components/Admin/admin/admin.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
 export class AdminComponent {
+  showModalMenuAdmin() {
+    let modalBg = document.querySelector('.modal-bg');
 
+    modalBg?.classList.add('modal-bg-active');
+  }
+  closeModalMenuAdmin() {
+    let modalBg = document.querySelector('.modal-bg');
+
+    modalBg?.classList.remove('modal-bg-active');
+  }
 }
