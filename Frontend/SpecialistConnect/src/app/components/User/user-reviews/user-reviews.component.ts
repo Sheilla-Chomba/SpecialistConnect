66487fa06538c6b6b94ddbff3cf5b4160ae1ed15
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-14 22:25:56 +0300

    User-reviews Finished

--- Frontend/SpecialistConnect/src/app/components/User/user-reviews/user-reviews.component.ts
+++ Frontend/SpecialistConnect/src/app/components/User/user-reviews/user-reviews.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-reviews',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-reviews.component.html',
-  styleUrl: './user-reviews.component.css'
+  styleUrl: './user-reviews.component.css',
 })
 export class UserReviewsComponent {
-
+  
 }
