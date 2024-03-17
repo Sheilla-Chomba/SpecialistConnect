7d81f88bf22dfcd18751cc3ee452cd9d6dd1f542
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-17 21:09:55 +0300

    Navigation added

--- Frontend/SpecialistConnect/src/app/components/User/user-view-spec/user-view-spec.component.ts
+++ Frontend/SpecialistConnect/src/app/components/User/user-view-spec/user-view-spec.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-view-spec',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-view-spec.component.html',
   styleUrl: './user-view-spec.component.css'
 })
