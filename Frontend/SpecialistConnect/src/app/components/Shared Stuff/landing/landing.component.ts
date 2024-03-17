992c338540f064a21f589037c39d6f71b6b2bbe6
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-17 23:24:02 +0300

    Landing continued

--- Frontend/SpecialistConnect/src/app/components/Shared Stuff/landing/landing.component.ts
+++ Frontend/SpecialistConnect/src/app/components/Shared Stuff/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [FooterComponent,NavbarComponent],
+  imports: [FooterComponent,NavbarComponent,RouterLink],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
