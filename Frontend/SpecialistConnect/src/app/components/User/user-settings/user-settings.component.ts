ce1940608b2c9bd73563ccf8998b36a61b4af9e6
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-21 11:16:45 +0300

    User settings working

--- Frontend/SpecialistConnect/src/app/components/User/user-settings/user-settings.component.ts
+++ Frontend/SpecialistConnect/src/app/components/User/user-settings/user-settings.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserServiceService } from '../../../services/User-Service/user-service.service';
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './user-settings.component.html',
-  styleUrl: './user-settings.component.css'
+  styleUrl: './user-settings.component.css',
 })
 export class UserSettingsComponent {
+  settingsForm!: FormGroup;
+  successMessage: string = '';
+  showSuccessMessage: boolean = false;
 
+   constructor(private fb: FormBuilder, private route: Router, public api:UserServiceService) {
+    this.settingsForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        pwd: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPwd: ['', [Validators.required, Validators.minLength(6)]],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+  passwordMatchValidator(formGroup: FormGroup) {
+    const passwordControl = formGroup.get('pwd');
+    const confirmPasswordControl = formGroup.get('confirmPwd');
+
+    if (passwordControl && confirmPasswordControl) {
+      const password = passwordControl.value;
+      const confirmPassword = confirmPasswordControl.value;
+
+      if (password !== confirmPassword) {
+        confirmPasswordControl.setErrors({ mismatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    }
+  }
+
+  settings(){
+    let newPassword = {
+      email: this.settingsForm.value.email,
+      password: this.settingsForm.value.pwd,
+    };
+    this.api.passwordReset(newPassword).subscribe((response) => {
+      console.log(response.message);
+
+      setTimeout(() => {
+        this.settingsForm.reset();
+        this.route.navigate(['/login']);
+      }, 3000);
+    });
+  }
 }
